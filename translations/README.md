2d002d506a9f3e683fdac257fbbf694cf0be3bca
Doug Beck <doug@douglasbeck.com>
2014-02-26 18:01:47 -0500

    simple php translated app

---
+++ translations/README.md
@@ -0,0 +1,9 @@
+# Translations
+
+## make messages
+
+`translations/make-messages.sh`
+
+## view output
+
+`php translations/app/helloworld.php`
--- translations/app/helloworld.php
+++ translations/app/helloworld.php
@@ -1,4 +1,7 @@
 <?php
 
+#putenv("LANG=es.utf8");
+bindtextdomain('messages', dirname(__FILE__).'/locale');
+
 echo _("Hello World");
 echo "\n";
