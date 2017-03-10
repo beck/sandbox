81982436b78eaee92d5d88b07d26d57be6246758
Doug Beck <doug@douglasbeck.com>
2017-03-09 20:47:19 -0500

    Example Bar!

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='example-bar',
+    version='1.0.0',
+    url='https://github.com/beck/sandbox/tree/example-bar',
+    install_requires=[
+        "simplejson < 3"
+    ]
+)
