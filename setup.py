4574bf050b433b6d595bb7c79cf35adc172c488c
Doug Beck <doug@douglasbeck.com>
2017-05-03 16:50:04 -0400

    Bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Doug Beck',
     author_email='doug@douglasbeck.com',
     name='example-foo',
-    version='1.0.2',
+    version='2.0.0',
     url='https://github.com/beck/sandbox/tree/example-foo',
     install_requires=[
         'example-bar == 1.0.1'
