2a99e1a14197242ac53aa29371377c95e2e23569
Doug Beck <doug@douglasbeck.com>
2017-05-03 16:44:09 -0400

    Require example-bar

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
     author='Doug Beck',
     author_email='doug@douglasbeck.com',
     name='example-foo',
-    version='1.0.1',
+    version='1.0.2',
     url='https://github.com/beck/sandbox/tree/example-foo',
+    install_requires=[
+        'example-bar == 1.0.1'
+    ]
 )
